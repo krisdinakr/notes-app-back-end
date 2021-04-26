597052e9776d1feef382baee2ca312b00b73d5c8
krisdinakr <krssdina@gmail.com>
2021-04-26 17:01:44 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,41 @@
+// kriteria 1
+// web server harus bisa menyimpan catatan yg ditambahkan dari app client
+// membuat fungsi menyimpan catatan
+
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
